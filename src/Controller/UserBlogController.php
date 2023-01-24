abab8afbb2d095ef6bc0672e45973cac81bf9a6c
JulienGrade <gradejulien@gmail.com>
2023-01-24 12:12:45 +0100

    Add create for user_blog

--- src/Controller/UserBlogController.php
+++ src/Controller/UserBlogController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Blog;
+use App\Form\BlogType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,11 +16,48 @@ class UserBlogController extends AbstractController
      * Permet d'afficher la page de gestion de blog dans mon espace
      * @param EntityManagerInterface $entityManager
      * @return Response
+     * @throws \Exception
      */
     #[Route('/mon-espace/mes-blogs', name: 'user_blog')]
-    public function index(EntityManagerInterface $entityManager): Response
+    public function index(EntityManagerInterface $entityManager, Request $request, string $blogIllustrationDir): Response
     {
         $blogs = $entityManager->getRepository(Blog::class)->findAll();
+        $blog = new Blog();
+        $blogForm = $this->createForm(BlogType::class, $blog);
+        $blogForm->handleRequest($request);
+        if($blogForm->isSubmitted() && $blogForm->isValid()){
+            // On gère la date de création
+            $dateTimeZone = new \DateTimeZone('Europe/Paris');
+            $date = new \DateTimeImmutable('now', $dateTimeZone);
+            $blog->setCreatedAt($date);
+            $blog->setAuthor($this->getUser());
+
+            if($illustration = $blogForm['illustration']->getData()){
+                $illustrationFilename = bin2hex(random_bytes(6)).'.'.$illustration->guessExtension();
+                try {
+                    $illustration->move($blogIllustrationDir, $illustrationFilename);
+                } catch (FileException $e){
+                    $this->addFlash('error_upload_article_media_new', 'Une erreur est survenue lors de l\'upload de l\'image');
+                }
+                $blog->setIllustration($illustrationFilename);
+            }
+            $entityManager->persist($blog);
+            $entityManager->flush();
+
+
+            $this->addFlash(
+                'success_blog_create',
+                'Votre blog a bien été créé'
+            );
+
+            return $this->redirectToRoute('user_blog');
+        }
+
+        return $this->render('user_blog/index.html.twig', [
+            'blogForm' => $blogForm,
+            'current_menu' => 'accueil',
+            'current_sidebar' => 'blog'
+        ]);
 
         return $this->render('user_blog/index.html.twig', [
             'blogs' => $blogs,
--- src/Form/BlogType.php
+++ src/Form/BlogType.php
@@ -4,11 +4,13 @@ namespace App\Form;
 
 use App\Entity\Blog;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Image;
 
 class BlogType extends AbstractType
 {
@@ -19,27 +21,36 @@ class BlogType extends AbstractType
                 'label' => 'Le nom de votre blog',
                 'attr' => [
                     'placeholder' => 'Merci de saisir le nom de votre blog',
-                    'class' => 'block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40'
+                    'class' => 'form-input block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40'
                 ]
             ])
             ->add('description', TextareaType::class, [
                 'label' => 'Description du blog',
                 'attr' => [
-                    'placeholder' => 'saisir la description du blog',
-                    'class' => 'block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40'
+                    'placeholder' => 'Saisir la description de ce blog',
+                    'class' => 'form-input block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40'
                 ]
             ])
-            ->add('illustration', TextType::class, [
-                'label' => 'L\'image du blog',
+            ->add('illustration', FileType::class, [
+                'required' => true,
+                'mapped' => false,
+                'label' => 'Image illustrant le blog',
                 'attr' => [
-                    'placeholder' => 'Merci de saisir l\'image de votre blog',
-                    'class' => 'block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40'
+                    'class' => 'form-file border mt-1 rounded py-4 px-4 w-full lg:w-1/2 mx-auto',
+                    'placeholder' => 'Importer une image pour ce blog',
+                ],
+                'constraints' => [
+                    new Image([
+                        'maxSize' => '2048k',
+                        'mimeTypes' => ["image/png", "image/jpeg", "image/pjpeg", "image/svg+xml"],
+                        'mimeTypesMessage' => "Formats d'image supportés : .jpg, .png, .jpeg, .svg, .mp4"
+                    ])
                 ]
             ])
             ->add('submit', SubmitType::class, [
                 'label' => 'Enregistrer',
                 'attr' => [
-                    'class' => 'my-8 w-full py-2 px-4 tracking-wide text-white transition-colors duration-200 transform bg-main-blue rounded-md hover:text-main-blue hover:bg-main-white focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40'
+                    'class' => 'my-8 w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-main-blue rounded-md hover:text-main-blue hover:bg-main-light-blue focus:outline-none focus:bg-blue-400 focus:ring focus:ring-blue-300 focus:ring-opacity-50'
                 ]
             ])
         ;
