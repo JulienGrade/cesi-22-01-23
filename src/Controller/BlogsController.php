19e0f243a53a7be81f393547d32eff6f43dbb052
JulienGrade <gradejulien@gmail.com>
2023-01-25 13:36:16 +0100

    Add pagination service

--- src/Controller/BlogsController.php
+++ src/Controller/BlogsController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Articles;
 use App\Entity\Blog;
+use App\Services\PaginationServices;
 use Doctrine\ORM\EntityManagerInterface;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,14 +16,26 @@ class BlogsController extends AbstractController
     /**
      * Permet d'afficher la page des blogs
      * @param EntityManagerInterface $entityManager
+     * @param PaginationServices $pagination
+     * @param $page
      * @return Response
+     * @throws Exception
      */
-    #[Route('/les-blogs', name: 'blogs')]
-    public function index(EntityManagerInterface $entityManager): Response
+    #[Route('/les-blogs/{page<\d+>?1}', name: 'blogs', requirements: ["page" => "\d+"])]
+    public function index(EntityManagerInterface $entityManager, PaginationServices $pagination, $page): Response
     {
-        $blogs = $entityManager->getRepository(Blog::class)->findAll();
+        $pagination
+            ->setOrderBy(['createdAt' => 'DESC'])
+            ->setPage($page)
+            ->setLimit(1)
+            ->setEntityClass(Blog::class)
+            ->setCriteria([]);
+        $blogs = $pagination->getData();
         return $this->render('blogs/index.html.twig', [
             'blogs' => $blogs,
+            'pages' => $pagination->getPages(),
+            'page' => $page,
+            'pagination' => $pagination,
             'current_menu' => 'blog'
         ]);
     }
@@ -28,14 +43,26 @@ class BlogsController extends AbstractController
     /**
      * Permet d'afficher le détail d'un blog
      * @return Response
+     * @throws Exception
      */
-    #[Route('/les-blogs/{slug}', name: 'blog_show')]
-    public function show(EntityManagerInterface $entityManager, $slug)
+    #[Route('/les-blogs/{slug}/{page<\d+>?1}', name: 'blog_show', requirements: ["page" => "\d+"])]
+    public function show(EntityManagerInterface $entityManager, $slug, PaginationServices $pagination, $page)
     {
         $blog = $entityManager->getRepository(Blog::class)->findOneBySlug($slug);
+        $pagination
+            ->setOrderBy(['createdAt' => 'DESC'])
+            ->setPage($page)
+            ->setLimit(7)
+            ->setEntityClass(Articles::class)
+            ->setCriteria(['blog' => $blog]);
+        $articles = $pagination->getData();
 
         return $this->render('blogs/show.html.twig', [
             'blog' => $blog,
+            'articles' => $articles,
+            'pages' => $pagination->getPages(),
+            'page' => $page,
+            'pagination' => $pagination,
             'current_menu' => 'blog'
         ]);
     }
